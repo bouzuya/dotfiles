2a48a9a50787651359048a1d488dab9d636c7ecf
bouzuya <bouzuya@gmail.com>
2013-12-05 23:40:56 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dotfiles_dir=$HOME/.dotfiles
+
+if [ -d "$dotfiles_dir" ]
+then
+  echo 'already installed'
+  exit 1
+fi
+
+echo 'installing...'
+if ! which git
+then
+  echo 'please install "git"'
+  exit 1
+fi
+
+git clone git://github.com/bouzuya/dotfiles.git "$dotfiles_dir"
+cd "$dotfiles_dir"
+chmod +x ./scripts/*.sh
+./scripts/link.sh
+
---
+++ scripts/link.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script_dir=$(cd $(dirname $0); pwd)
+dotfiles_dir=$(dirname $script_dir)
+
+for f in $(ls $dotfiles_dir -A -I 'scripts' -I 'install.sh' -I '.git')
+do
+  if [ -f $HOME/$f ]
+  then
+    echo $HOME/$f already exists.
+  else
+    ln -s $dotfiles_dir/$f $HOME/$f
+  fi
+done
+
