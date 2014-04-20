57fbbb95267e4713eb30896340a20aa573ec5f56
bouzuya <bouzuya@gmail.com>
2014-04-20 16:57:21 +0900

    add .gitignore

--- scripts/list.sh
+++ scripts/list.sh
@@ -3,7 +3,7 @@
 script_dir=$(cd $(dirname $0); pwd)
 dotfiles_dir=$(dirname $script_dir)
 
-for f in $(ls $dotfiles_dir -A -I 'scripts' -I 'install.sh' -I '.git' -I '.gitmodules' -I 'README.md')
+for f in $(ls $dotfiles_dir -A -I 'scripts' -I 'install.sh' -I '.git' -I '.gitignore' -I '.gitmodules' -I 'README.md')
 do
   echo $f
 done
